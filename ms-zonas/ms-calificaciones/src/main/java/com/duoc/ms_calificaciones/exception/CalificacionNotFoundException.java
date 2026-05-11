7716e2035c5a69aa77a769c9c27b27ecf2fd0f14
Ignacio Bravo <ignacioalberto48@gmail.com>
2026-05-10 22:09:16 -0400

    fix: corregir puertos repetidos y carpetas basura

--- ms-zonas/ms-calificaciones/src/main/java/com/duoc/ms_calificaciones/exception/CalificacionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.duoc.ms_calificaciones.exception;
-
-public class CalificacionNotFoundException extends RuntimeException {
-
-    public CalificacionNotFoundException(Long id) {
-        super("No se encontró una calificación con el id: " + id);
-    }
-}
--- ms-zonas/ms-incidencias/src/main/java/com/duoc/ms_incidencias/exception/IncidenciaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.duoc.ms_incidencias.exception;
-
-public class IncidenciaNotFoundException extends RuntimeException {
-
-    public IncidenciaNotFoundException(Long id) {
-        super("No se encontró una incidencia con el id: " + id);
-    }
-}
